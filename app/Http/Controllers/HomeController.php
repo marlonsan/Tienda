60726256b55c108fcc3f49e42c9e1fc51f646d2d
marlonsan <marlon.sanchez.chavez@gmail.com>
2017-11-13 20:38:28 -0500

    Agregue las vistas condiciones y nosotros

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->only('perfil');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function condiciones()
+    {
+        return view('condiciones');
+    }
+
+    public function nosotros()
+    {
+        return view('nosotros');
+    }
+}
