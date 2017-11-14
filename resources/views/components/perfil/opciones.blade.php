83060f81165c22f439b30083843800141bc4a796
marlonsan <marlon.sanchez.chavez@gmail.com>
2017-11-14 00:24:17 -0500

    Cree una vista opciones del perfil

---
+++ resources/views/components/perfil/opciones.blade.php
@@ -0,0 +1,21 @@
+<div class="grupo_opciones_perfil">
+	<div class="titulo_item_perfil">Opciones de tu perfil</div>
+	<div class="item_perfil">
+		<a href="">
+			<img src="{{ asset('img/icon_perfil/user.png') }}">
+			<p>Datos Personales</p>
+		</a>
+	</div>
+	<div class="item_perfil">
+		<a href="">
+			<img src="{{ asset('img/icon_perfil/promociones.png') }}">
+			<p>Productos Comprados</p>
+		</a>
+	</div>
+	<div class="item_perfil">
+		<a href="">
+			<img src="{{ asset('img/icon_perfil/contraseña.png') }}">
+			<p>Reestablecer contraseña</p>
+		</a>
+	</div>
+</div>
